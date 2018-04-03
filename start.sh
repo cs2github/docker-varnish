d19653f4b7f33ccb435c5c2cc5a8ef97d87718df
Stefan Preissner <stefan.preissner@cs2.ch>
2018-04-03 15:03:15 +0200

    Disable varnishlog / Run Varnish in Foreground

--- start.sh
+++ start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 # Start varnish
-varnishd -f /etc/varnish/default.vcl -S /etc/varnish/secret -s ${VARNISH_STORAGE} ${VARNISH_PARAMS} -a 0.0.0.0:${VARNISH_PORT} -T 0.0.0.0:6082
-# Somehow required, otherwise it fails to start...
-sleep 1
-# Start varnishlog and keep container running
-varnishlog
+varnishd -F -f /etc/varnish/default.vcl -S /etc/varnish/secret -s ${VARNISH_STORAGE} ${VARNISH_PARAMS} -a 0.0.0.0:${VARNISH_PORT} -T 0.0.0.0:6082
+
